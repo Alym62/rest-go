540ff90e73b65a9633f6d686f5f27abde4681e2f
Alym62 <gasafm62@gmail.com>
2024-05-08 10:03:00 -0300

    feat: adicionado método de delete. Funcionando com sucesso.

--- handler/method/deleteHandler.go
+++ handler/method/deleteHandler.go
@@ -1,13 +1,34 @@
 package method
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Alym62/rest-go/handler/response"
+	"github.com/Alym62/rest-go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteHandler(context *gin.Context) {
-	context.JSON(http.StatusNoContent, gin.H{
-		"message": "Primeira API com Go",
-	})
+	id := context.Query("id")
+	if id == "" {
+		response.SendError(context, http.StatusBadRequest, "id is not empyt")
+		return
+	}
+
+	product := schemas.Product{}
+
+	// TODO: Find product
+	if err := db.First(&product, id).Error; err != nil {
+		response.SendError(context, http.StatusNotFound, fmt.Sprintf("product with id: %s not found", id))
+		return
+	}
+
+	// TODO: Delete product
+	if err := db.Delete(&product).Error; err != nil {
+		response.SendError(context, http.StatusInternalServerError, fmt.Sprintf("error deleting product with id: %s", id))
+		return
+	}
+
+	response.SendSuccess(context, "delete-product", product)
 }

b1c7148664a8a9955a8d96c354eabdacfa62aaa5
Alym62 <gasafm62@gmail.com>
2024-05-11 10:45:28 -0300

    feat: adicionado o swagger do projeto. Foi feito alguns ajustes por questões de configuração da documentação.

--- router/routes.go
+++ router/routes.go
@@ -1,15 +1,21 @@
 package router
 
 import (
+	docs "github.com/Alym62/rest-go/docs"
 	"github.com/Alym62/rest-go/handler/method"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// TODO: Initializar request
 	method.InitializerHandlerLogger()
 
-	v1 := router.Group("/api/v1/ws")
+	basePath := "/api/v1/ws"
+	docs.SwaggerInfo.BasePath = basePath
+
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/", method.FindHandler)
 		v1.GET("/find", method.FindByIdHandler)
@@ -17,4 +23,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/update", method.UpdateHandler)
 		v1.DELETE("/delete", method.DeleteHandler)
 	}
+
+	// TODO: Initializer swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }

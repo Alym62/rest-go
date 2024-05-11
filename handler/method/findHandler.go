db60987a7213cde4508563324a4a23f5f309b3cf
Alym62 <gasafm62@gmail.com>
2024-05-10 22:21:15 -0300

    feat: adicionado o método de buscar os dados no db.

--- handler/method/findHandler.go
+++ handler/method/findHandler.go
@@ -3,11 +3,18 @@ package method
 import (
 	"net/http"
 
+	"github.com/Alym62/rest-go/handler/response"
+	"github.com/Alym62/rest-go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func FindHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "Primeira API com Go",
-	})
+	products := []schemas.Product{}
+
+	if err := db.Find(&products).Error; err != nil {
+		response.SendError(context, http.StatusInternalServerError, "Error listing products")
+		return
+	}
+
+	response.SendSuccess(context, "list-products", products)
 }
